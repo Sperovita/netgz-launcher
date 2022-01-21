1d32b418bd75ec50ebd823ed494c4bc9d253a159
Greg Ransons <gregransons@gmail.com>
2022-01-21 13:34:56 -0700

    modfile slimselect hook

--- src/renderer.js
+++ src/renderer.js
@@ -11,7 +11,7 @@ function data() {
 			hostJoin: 'join',
 			ip: '',
 			port: '',
-			modFiles: [],
+			modFiles: ['test.pke'],
 			additionalCommands: '',
 			private: true,
 			players: 2,
@@ -20,9 +20,11 @@ function data() {
 			mode: 'coop',
 			netmode: '0',
 		},
+		modSlimSelect: null,
 		command(){
 			const cc = this.cc;
 			let command = `gzdoom -${cc.hostJoin}`;
+
 			if(cc.hostJoin === 'host'){
 				command += ` ${cc.players} ${cc.private ? '-private' : ''} -netmode ${cc.netmode}`;
 				if(cc.skill > -1){
@@ -37,9 +39,11 @@ function data() {
 			}else if(cc.hostJoin === 'join'){
 				command += ` ${cc.ip}`;
 			}
+
 			if(cc.port){
 				command += ` -port ${cc.port}`;
 			}
+
 			if(cc.modFiles.length){
 				command += ` -file`;
 				cc.modFiles.forEach(file => {
@@ -52,7 +56,7 @@ function data() {
 			return command;
 		},
 		configChanged(){
-			console.log('config changed')
+			this.modSlimSelect.set(this.cc.modFiles);
 		},
 		save(){
 
@@ -76,12 +80,19 @@ function data() {
 				const cs = new SlimSelect({
 					select: '#config_selector'
 				});
-				const ms = new SlimSelect({
+
+				// slim select multi doesn't work well with alpine, need to rely on a more classic approach
+				this.modSlimSelect = new SlimSelect({
 					select: '#mod_selector',
-					placeholder: 'Select Mod Files'
+					placeholder: 'Select Mod Files',
+					data: this.allModFiles.map(file => ({text: file}) ),
+					onChange: (info) => {
+						this.cc.modFiles = info.map(choice => choice.value);
+					},
 				});
+				this.modSlimSelect.set(this.cc.modFiles);
 			}catch(error){
-				// This library has an annoying bug currently
+				// This library has an annoying bug on render currently
 			}
 		}
 	}
